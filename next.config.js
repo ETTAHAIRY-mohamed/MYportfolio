9ec14be1405cabbf3cdfffa132b0ab3e94b9686b
Ettahairy_Mohamed <mohamed.ettahairy@usmba.ac.ma>
2025-06-02 16:26:34 +0100

    Configure GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -1,8 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  basePath: '/porfolio-1',
+  images: {
+    unoptimized: true,
+  },
   experimental: {
     appDir: true,
   },
 };
 
 module.exports = nextConfig;
+
+
